45fde866c7f1e053864e804cb1858638dae1dbe6
farisbrandone <farisbrandone@yahoo.com>
2025-03-25 11:13:45 +0100

    aa

--- src/page/lib/createUser.js
+++ src/page/lib/createUser.js
@@ -19,8 +19,7 @@ export const createUser = async (email, motsDepasse, auth) => {
     };
   }
 };
-export const baseUrl =
-  "http://127.0.0.1:5001/un-truc-de-jesus-carte/us-central1/adminnode";
+export const baseUrl = "https://adminnode-adzkyuspnq-uc.a.run.app";
 
 /* 
 http://127.0.0.1:5001/un-truc-de-jesus-carte/us-central1/adminnode
